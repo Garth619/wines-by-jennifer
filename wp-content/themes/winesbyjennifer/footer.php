d07c135d2fd1f4f18600d813b29ff57bd0798d1c
garrett cullen <info@garrettcullen.com>
2016-01-01 12:55:26 -0800

    push

--- wp-content/themes/winesbyjennifer/footer.php
+++ wp-content/themes/winesbyjennifer/footer.php
@@ -14,16 +14,25 @@
 </div><!-- main -->
 </div><!-- wrapper -->
 <div class="footer">
-	<ul class="menu">
-		<li><a href="">Contact</a></li>
-		<li><a href="">FAQ</a></li>
-		<li><a href="">Privacy</a></li>
-		<li><a href="">Please Drink Responsibly</a></li>
-	</ul>
-	<ul style="clear:both;">
-		<li>View In: Eng ESp</li>
-		<li>c 2015 Wines By Jennifer r</li>
-	</ul>
+	<div class="footer_col1">
+		<ul class="desktop_copyright">
+			<li>c 2015 Wines By Jennifer r</li>
+		</ul><!-- desktop_copyright -->
+	</div><!-- footer_col1 -->
+	<div class="footer_col2">
+		<ul class="menu">
+			<li><a href="">Contact</a></li>
+			<li><a href="">FAQ</a></li>
+			<li><a href="">Privacy</a></li>
+			<li><a href="">Please Drink Responsibly</a></li>
+		</ul>
+	</div><!-- footer_col2 -->
+	<div class="footer_col3">
+		<ul class="language" style="clear:both;">
+			<li>View In: Eng ESp</li>
+			<li>c 2015 Wines By Jennifer r</li>
+		</ul>
+	</div><!-- footer_col3 -->
 </div><!-- footer -->
 
 
