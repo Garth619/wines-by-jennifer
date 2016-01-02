f9abb9cb0698ecf1f78b360de8c2a5ddec71223f
garrett cullen <info@garrettcullen.com>
2016-01-01 19:36:44 -0800

    push

--- wp-content/themes/winesbyjennifer/page-main.php
+++ wp-content/themes/winesbyjennifer/page-main.php
@@ -18,12 +18,39 @@ get_header(); ?>
 	<div class="main_top">
 		
 		<div class="desktop_testimomials">
+			<div class="testimonial_top_half">
+				<span class="testimonial_quote purple_quote">"</span><p class="tesimonial_content">Thank you for helping make our wedding and reception absolutely perfect! We were able to relax, enjoy the evening with loved ones, as well as enjoy the outstanding food and drink. It was truly a surreal and  magical&nbsp;evening.<span class="purple_quote">"</p>
+				<img class="testimonial_img" src="<?php bloginfo('template_directory');?>/images/circle.png"/>
+				<div class="testimonial_name">
+					<p>Sasha & Ryan Kalis</p>
+					<p>Parkville, Missouri</p>
+				</div><!-- testimonial_name -->
+			</div><!-- testimonial_top_half -->
+			<div class="testimonial_bottom_half">
+			<div class="divider"></div><!-- divider -->
 			
-			<p>"Thank you for helping make our wedding and reception absolutely perfect! We were able to relax, enjoy the evening with loved ones, as well as enjoy the outstanding food and drink. It was truly a surreal and  magical evening."</p>
-			<img class="testimonial_img" src="<?php bloginfo('template_directory');?>/images/circle.png"/>
-			<div class="divider">
-				
-			</div><!-- divider -->
+				<div class="review_icons">
+						
+						<div class="review_single_icon">
+							<a href=""><img src="<?php bloginfo('template_directory');?>/images/yelp.jpg"/></a>
+						</div><!-- review_single_icon -->
+						<div class="review_single_icon">
+							<a href=""><img src="<?php bloginfo('template_directory');?>/images/trip-advisor.png"/></a>
+						</div><!-- review_single_icon -->
+						<div class="review_single_icon">
+							<a href=""><img src="<?php bloginfo('template_directory');?>/images/google.png"/></a>
+						</div><!-- review_single_icon -->
+						
+				</div><!-- review_icons -->
+			
+			<div class="green_bar">
+				<a href="">
+					<img src="<?php bloginfo('template_directory');?>/images/stars.png"/>LEAVE A REVIEW<img src="<?php bloginfo('template_directory');?>/images/stars.png"/>
+				</a>
+			</div><!-- green_bar -->
+			
+		</div><!-- testimonial_bottom_half -->
+			
 			
 		</div><!-- desktop_testimomials -->
 		
