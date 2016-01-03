d5588fb8e27bf523184a803c1031fb08e89033ca
garrett cullen <info@garrettcullen.com>
2016-01-03 10:23:05 -0800

    push

--- wp-content/themes/winesbyjennifer/page-meettheteam.php
+++ wp-content/themes/winesbyjennifer/page-meettheteam.php
@@ -96,19 +96,23 @@ get_header(); ?>
 			<div class="left_col_bio">
 				
 				<img class="team_profile" src="<?php bloginfo('template_directory');?>/images/homer.jpg"/>
-				<div class="arrow-right-purple-left"></div>
-				<span class="back_to_bio">Back to Bios</span><!-- back_to_bio -->
-				
+				<div class="back_to_bio_wrapper">
+					<div class="arrow-right-purple-left"></div>
+					<span class="back_to_bio">Back to Bios</span><!-- back_to_bio -->
+				</div><!-- back_to_bio_wrapper -->
 				
 			</div><!-- left_col_bio -->
 			
 			
 			<div class="right_col_bio">
-				<span class="bio_close">Close X</span>
+				
 				<h1>Member Name</h1>
+				<span class="bio_close">Close Bio X</span>
+				<div class="bio_content">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et</p> 
+				</div><!-- bio_content -->
 				<div class="bio_buttons">
 					<a href="" class="bio_back"><div class="arrow-right-purple-left"></div> Meet (Back)</a>	
 					<a href="" class="bio_next">Meet (Back) <div class="arrow-right-purple"></div></a>
