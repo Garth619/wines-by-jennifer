389a201b6b06400e75b38c6f46605be0aa842484
garrett cullen <info@garrettcullen.com>
2016-01-11 21:57:04 -0800

    push

--- wp-content/themes/winesbyjennifer/single-inthenews.php
+++ wp-content/themes/winesbyjennifer/single-inthenews.php
@@ -16,7 +16,7 @@ get_header(); ?>
 	
 	<div class="banner_overlay">
 	
-		<h1><img class="ribbon ribbon_left" src="<?php bloginfo('template_directory');?>/images/ribbon.png"/><?php the_title();?><img class="ribbon ribbon_right" src="<?php bloginfo('template_directory');?>/images/ribbon-right.png"/></h1>
+		<h1><img class="ribbon ribbon_left" src="<?php bloginfo('template_directory');?>/images/ribbon.png"/>In the News<img class="ribbon ribbon_right" src="<?php bloginfo('template_directory');?>/images/ribbon-right.png"/></h1>
 	
 	</div><!-- banner_overlay -->
 	
