d2f484b342b654378311fb15134151fdd09856a2
garrett cullen <info@garrettcullen.com>
2016-02-03 21:16:21 -0800

    push

--- wp-content/themes/winesbyjennifer/page-main.php
+++ wp-content/themes/winesbyjennifer/page-main.php
@@ -20,47 +20,40 @@ get_header(); ?>
 		<div class="desktop_testimomials">
 			<div class="testimonial_top_half">
 		
-				<div style="width:100%;" class="cycle-slideshow" data-cycle-slides="> .test_wrapper"  data-cycle-timeout=0  data-cycle-prev=".test-prev"
+				<div style="width:100%;" class="cycle-slideshow" data-cycle-slides="> .test_wrapper"  data-cycle-timeout=5000  data-cycle-prev=".test-prev"
         data-cycle-next=".test-next">
 			
 				<div class="test_wrapper">
 					<span class="testimonial_quote purple_quote">"</span><p class="tesimonial_content">Thank you for helping make our wedding and reception absolutely perfect! We were able to relax, enjoy the evening with loved ones, as well as enjoy the outstanding food and drink. It was truly a surreal and  magical&nbsp;evening.<span class="purple_quote">"</span></p>
 					<img class="testimonial_img" src="<?php bloginfo('template_directory');?>/images/circle.png"/>
 					<div class="testimonial_name">
-						<p>Sasha & Ryan Kalis</p>
+						<p>Ryan Kalis</p>
 						<p>Parkville, Missouri</p>
 					</div><!-- testimonial_name -->
 				</div><!-- test_wrapper -->
 				
 				
 				<div class="test_wrapper">
-					<span class="testimonial_quote purple_quote">"</span><p class="tesimonial_content">Thank you for helping make our wedding and reception absolutely perfect! We were able to relax, enjoy the evening with loved ones, as well as enjoy the outstanding food and drink. It was truly a surreal and  magical&nbsp;evening.<span class="purple_quote">"</span></p>
+					<span class="testimonial_quote purple_quote">"</span><p class="tesimonial_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <span class="purple_quote">"</span></p>
 					<img class="testimonial_img" src="<?php bloginfo('template_directory');?>/images/circle.png"/>
 					<div class="testimonial_name">
-						<p>yoo & Ryan Kalis</p>
-						<p>Parkville, Missouri</p>
+						<p>Name</p>
+						<p>Location</p>
 					</div><!-- testimonial_name -->
 				</div><!-- test_wrapper -->
 				
-				
-				<div class="test_wrapper">
-					<span class="testimonial_quote purple_quote">"</span><p class="tesimonial_content">Thank you for helping make our wedding and reception absolutely perfect! We were able to relax, enjoy the evening with loved ones, as well as enjoy the outstanding food and drink. It was truly a surreal and  magical&nbsp;evening.<span class="purple_quote">"</span></p>
-					<img class="testimonial_img" src="<?php bloginfo('template_directory');?>/images/circle.png"/>
-					<div class="testimonial_name">
-						<p>Sasha & Ryan Kalis</p>
-						<p>Parkville, Missouri</p>
-					</div><!-- testimonial_name -->
+
 					
 	
 				</div><!-- cycle-slideshow -->
 				
-				<div class="test-prev">prev</div>
-				<div class="test-next">next</div>
+				<div class="arrow-left-purple-test test-prev"></div>
+				<div class="arrow-right-purple-test test-next"></div>
 					
-				</div><!-- test_wrapper -->
+			
 			</div><!-- testimonial_top_half -->
 			<div class="testimonial_bottom_half">
-			<div class="divider"></div><!-- divider -->
+			<div style="margin-bottom:5px" class="divider"></div><!-- divider -->
 			
 				<div class="review_icons">
 						
