27d526ff56bcd2d8c8d687431ddc4ec2d59978cf
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-03-07 21:31:58 -0800

    Push

--- wp-content/themes/winesbyjennifer/header.php
+++ wp-content/themes/winesbyjennifer/header.php
@@ -58,7 +58,7 @@ header('location:' . $_SERVER['REQUEST_URI'] . '?display=' . $_SESSION['display'
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=3" />
 <link href='https://fonts.googleapis.com/css?family=Cinzel:400,700|Lato:400,400italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/animate.css">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
