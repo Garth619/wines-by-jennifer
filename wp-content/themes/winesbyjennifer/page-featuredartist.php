8ab585f6b21f16e6aaa3ddcbb6f863e5a5f32206
Garrett Cullen <Garth619@users.noreply.github.com>
2019-01-01 19:30:02 -0800

    push

--- wp-content/themes/winesbyjennifer/page-featuredartist.php
+++ wp-content/themes/winesbyjennifer/page-featuredartist.php
@@ -13,6 +13,39 @@ get_header(); ?>
 	
 	<div id="content">
 		
+		<div class="gallery_intro">
+			
+			
+			<p>Come relax with your food and wine in our Art Gallery, which comprises several intimate seating areas in the lower level of Wines by Jennifer, each adorned with beautiful artwork from our Featured Artist. Our Featured Artists rotate on a regular basis, so there are always fresh and interesting works for you to enjoy or purchase.</p>
+
+<p>In addition to the Featured Artwork, we also have on permanent display and for sale several beautiful ceramic and woodworking pieces.</p>
+			
+			<div class="gallery_images">
+				
+				<?php if(get_field('featured_art_images')): ?>
+				 
+					<?php while(has_sub_field('featured_art_images')): ?>
+				 
+						<div class="single_gallery">
+					
+							<?php $images = get_sub_field( 'images' ); ?>
+							
+							<?php if ( $images ) { ?>
+							
+							<img src="<?php echo $images['url']; ?>" alt="<?php echo $images['alt']; ?>" />
+							
+							<?php } ?>
+					
+						</div><!-- single_gallery -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
+				
+			</div><!-- gallery_images -->
+			
+		</div><!-- gallery_intro -->
+		
 	<?php
 
 $post_object = get_field('featured_artist_featured_post');
